609f11d40bae8aa3ae315bd892a33f8953db99f9
tncommerce <tuanaicommerce@gmail.com>
2026-09-18 20:31:14 +0200

    Add launch-safe SCENTAI robots metadata

---
+++ examples/retail/storefront-web/app/robots.ts
@@ -0,0 +1,23 @@
+import type { MetadataRoute } from "next";
+
+import { SITE_INDEXABLE, SITE_URL } from "@/lib/site";
+
+export default function robots(): MetadataRoute.Robots {
+  if (!SITE_INDEXABLE) {
+    return {
+      rules: {
+        userAgent: "*",
+        disallow: "/",
+      },
+    };
+  }
+
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
+    sitemap: `${SITE_URL}/sitemap.xml`,
+    host: SITE_URL,
+  };
+}
