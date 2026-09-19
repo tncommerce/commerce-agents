73550c0391e6d4a5cd7c441640066b4cf0048e0d
tncommerce <tuanaicommerce@gmail.com>
2026-09-19 11:46:13 +0200

    Refresh Jarvis image state from global asset registry

--- scripts/refresh_scentai_jarvis_state.py
+++ scripts/refresh_scentai_jarvis_state.py
@@ -34,7 +34,7 @@ OFFERS = DATA_DIR / "merchant_offers.json"
 RELEASE_01 = DATA_DIR / "scentai_release_batch_01.json"
 RELEASE_02 = DATA_DIR / "scentai_release_batch_02.json"
 RELEASE_03 = DATA_DIR / "scentai_release_batch_03.json"
-ASSET_CANDIDATES = DATA_DIR / "scentai_release_01_asset_candidates.json"
+ASSET_CANDIDATES = DATA_DIR / "scentai_image_asset_candidates.json"
 
 OUT_MAPPING = DATA_DIR / "scentai_merchant_mapping_work_queue.json"
 OUT_AFFILIATE = DATA_DIR / "scentai_affiliate_activation_status.json"
